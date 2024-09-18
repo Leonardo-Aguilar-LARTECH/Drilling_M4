3a35303ef0f69c12416e211eccc42a6e3ad5c808
Leonardo Aguilar R. <leonardo1621@e-camp.cl>
2024-09-18 12:26:36 -0300

    correcciones 3

--- main.py
+++ main.py
@@ -7,20 +7,7 @@ repetir = False
 lg.leer_automoviles()
 
 while not repetir:
-    print(lg.listado_automoviles)
-    print(lg.listado_bicicletas)
-    print(lg.listado_veh_carga)
-    print("Particulares:",lg.listado_veh_particular)
-    print(lg.listado_vehiculos)
-    print(lg.listado_motocicletas)
-    time.sleep(5)
     repetir = lg.ingreso_vehiculos()
     if repetir:
-        salida = input("desea continuar (s)?")
-        if salida.lower() == "s":
-            repetir = False
-        else:
-            lg.clear_screen()
-            lg.imprimir_automoviles()
-            lg.guardar_automoviles()
-            repetir = True
+        lg.guardar_automoviles()
+        repetir = True
